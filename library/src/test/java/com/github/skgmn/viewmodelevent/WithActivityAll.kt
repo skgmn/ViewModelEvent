90899d33bc7b823c34eed30bfe6ce9359d92dd8f
suckgamony <suckgamony@gmail.com>
2021-08-06 20:57:40 +0900

    add test

--- library/src/test/java/com/github/skgmn/viewmodelevent/WithActivityAll.kt
+++ library/src/test/java/com/github/skgmn/viewmodelevent/WithActivityAll.kt
@@ -15,7 +15,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class WithActivityBuffer {
+class WithActivityAll {
     @get:Rule
     val activityScenarioRule = activityScenarioRule<TestActivity>()
 
